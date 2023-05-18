50b0820a72639bfc4a4075a5d359b55e9184c5eb
MiguelP-10 <129193535+MiguelP-10@users.noreply.github.com>
2023-05-18 10:21:48 +0100

    p6.4

---
+++ src/PaqCalc/mostrarlistener.java
@@ -0,0 +1,14 @@
+package PaqCalc;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mostrarlistener  {
+
+
+  public void hola(){
+
+  }
+}
+
